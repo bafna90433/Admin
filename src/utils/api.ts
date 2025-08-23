55eee2ceaa96541cd5b400708a0600d341349f04
BafnaToys <bafnatoysphotos@gmail.com>
2025-08-23 12:21:21 +0530

    fix: use VITE_API_URL env variable for api base url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:5000/api",
+  baseURL: process.env.VITE_API_URL as string,   // 👈 yahi line
 });
 
 api.interceptors.request.use((config) => {
